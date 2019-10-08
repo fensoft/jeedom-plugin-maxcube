398997df1f64edb124702f6d9ae458d7cbfde185
fensoft <fensoft@gmail.com>
2019-10-08 16:00:37 +0200

    removed nodejs5 for rpi1 (nomore supported), changed npm cache folder

--- resources/install_maxcube.sh
+++ resources/install_maxcube.sh
@@ -29,14 +29,10 @@ if [ `arch` == "armv6l" -a $major -lt 5 ]; then
   wget http://node-arm.herokuapp.com/node_latest_armhf.deb
   sudo dpkg -i node_latest_armhf.deb
   rm node_latest_armhf.deb
-elif [ `arch` == "aarch64" -a $major -lt 5 ]; then
-  wget http://dietpi.com/downloads/binaries/c2/nodejs_5-1_arm64.deb
-  sudo dpkg -i nodejs_5-1_arm64.deb
-  rm nodejs_5-1_arm64.deb
 elif [ $major -lt 8 ]; then
   echo "using official repository"
   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-  sudo apt-get install -y nodejs
+  sudo apt-get install -y nodejs npm
 fi
 
 BIN=`which node`
@@ -49,7 +45,8 @@ echo "new version installed: ${new}"
 echo 50 > /tmp/maxcube_in_progress
 git --version
 cd $ME/maxcube.js
-sudo rm -rf node_modules ~/.npm
+sudo rm -rf node_modules
+echo "cache = \"$ME/npm-cache\"" > .npmrc
 npm i
 echo 100 > /tmp/maxcube_in_progress
 rm /tmp/maxcube_in_progress
